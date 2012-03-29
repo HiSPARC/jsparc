b75a7ce1b9ce5a9a78fda636109ee60b7c09ebd9
Arne de Laat <adelaat@nikhef.nl>
2012-03-29 10:28:24 +0200

    Improved spacing

--- jSparc.js
+++ jSparc.js
@@ -22,36 +22,34 @@
    energy of the shower have been found.
    The variable "data" is a JSON of the form:
 
-   {timestamp:"coincidence timestamp",
-    nanoseconds:"nanoseconds of coincidence timestamp",
-    events:[status:"statusinformation",
-            timestamp:"event timestamp",
-            nanoseconds:"nanoseconds of event"
-            lon:"longitude of station",
-            lat:"latitude of station",
-            alt:"altitude of station",
-            number:"numbers of station",
-            pulseheights:["the pulseheights of the trace 0/3 in mV"],
-            integrals:["the area above the trace in mVns"],
-            mips:["numper of mips for each detector"],
-            traces:[["arrays of data of the traces 0/3"]]
-           ],
-   }
+   {timestamp: "coincidence timestamp",
+    nanoseconds: "nanoseconds of coincidence timestamp",
+    events:[status: "statusinformation",
+            timestamp: "event timestamp",
+            nanoseconds: "nanoseconds of event"
+            lon: "longitude of station",
+            lat: "latitude of station",
+            alt: "altitude of station",
+            number: "numbers of station",
+            pulseheights: ["the pulseheights of the trace 0/3 in mV"],
+            integrals: ["the area above the trace in mVns"],
+            mips: ["numper of mips for each detector"],
+            traces: [["arrays of data of the traces 0/3"]]],}
 
-   The variable "htmlInfo" contains information of the script wich calls the function and gives
-   the names of input (output) instances, for intance:
+   The variable "htmlInfo" contains information of the script wich calls the
+   function and gives the names of input (output) instances, for intance:
 
-   {mapId:"id of the map",
-    distId:"id of the inputs for distances",
-    chartId:"id for the plotted charts",
-    mipId:"id for the input of the MIP-flux",
-    mipCalcId:"id for the input of the calculated flux",
-    energyId:"id for the input for the calculated energy",
-    stationEr:"id for the error of the energy using the station averages",
-    showerEr:"id for the input of the error using the detected values"};
+   {mapId: "id of the map",
+    distId: "id of the inputs for distances",
+    chartId: "id for the plotted charts",
+    mipId: "id for the input of the MIP-flux",
+    mipCalcId: "id for the input of the calculated flux",
+    energyId: "id for the input for the calculated energy",
+    stationEr: "id for the error of the energy using the station averages",
+    showerEr: "id for the input of the error using the detected values"};
 
-   Several functions to calculate the energie of the primary particle. The azimuth
-   dependency is not used!!
+   Several functions to calculate the energy of the primary particle.
+   The azimuth dependency is not used!!
 
 */
 
@@ -61,9 +59,9 @@
  * from: Vincenty inverse formula - T Vincenty, "Direct and Inverse Solutions of Geodesics on the *
  *       Ellipsoid with application of nested equations", Survey Review, vol XXII no 176, 1975    *
  *       http://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf                                             *
- * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-
-/**
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *
+ *
+ *
  * Calculates geodetic distance between two points specified by latitude/longitude using
  * Vincenty inverse formula for ellipsoids
  *
@@ -97,26 +95,26 @@ function distVincenty(lat1, lon1, lat2, lon2) {
 
   do {
     var sinLambda = Math.sin(lambda), cosLambda = Math.cos(lambda);
-    sinSigma = Math.sqrt((cosU2*sinLambda)*(cosU2*sinLambda)+(cosU1*sinU2-sinU1*cosU2*cosLambda)*(cosU1*sinU2-sinU1*cosU2*cosLambda));
+    sinSigma = Math.sqrt((cosU2 * sinLambda) * (cosU2 * sinLambda) + (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) * (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda));
     if (sinSigma===0) {return 0;}  // co-incident points
-    cosSigma = sinU1*sinU2+cosU1*cosU2*cosLambda;
-    sigma = Math.atan2(sinSigma,cosSigma);
-    var sinAlpha = cosU1*cosU2* sinLambda / sinSigma;
-    cosSqAlpha = 1-sinAlpha*sinAlpha;
-    cos2SigmaM = cosSigma-2*sinU1*sinU2/cosSqAlpha;
+    cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda;
+    sigma = Math.atan2(sinSigma, cosSigma);
+    var sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
+    cosSqAlpha = 1 - sinAlpha * sinAlpha;
+    cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha;
     if (isNaN(cos2SigmaM)) {cos2SigmaM=0;}  // equatorial line: cosSqAlpha=0 (§6)
-    var C = f/16*cosSqAlpha*(4+f*(4-3*cosSqAlpha));
+    var C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
     lambdaP = lambda;
-    lambda = L+(1-C)*f*sinAlpha*(sigma + C*sinSigma*(cos2SigmaM+C*cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)));
-  } while((Math.abs(lambda-lambdaP)>1e-12)&&(--iterLimit>0));
+    lambda = L + (1 - C) * f * sinAlpha * (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM)));
+  } while((Math.abs(lambda-lambdaP)>1e-12) && (--iterLimit>0));
 
   if (iterLimit===0) {return NaN;}  // formula failed to converge
 
-  var uSq = cosSqAlpha*(a*a-b*b)/(b*b);
-  var A = 1+uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)));
-  var B = uSq/1024*(256+uSq*(-128+uSq*(74-47*uSq)));
-  var deltaSigma = B*sinSigma*(cos2SigmaM+B/4*(cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)-B/6*cos2SigmaM*(-3+4*sinSigma*sinSigma)*(-3+4*cos2SigmaM*cos2SigmaM)));
-  var s = b*A*(sigma-deltaSigma);
+  var uSq = cosSqAlpha * (a * a - b * b) / (b * b);
+  var A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
+  var B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
+  var deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM)));
+  var s = b * A * (sigma - deltaSigma);
 
   s = s.toFixed(0); // round to 1m precision
   return s;
@@ -132,7 +130,7 @@ function distVincenty(lat1, lon1, lat2, lon2) {
 /* Code clean up by N.G.Schultheiss using http://jslint.com/                                           */
 
 var showerMerc, shower4326;
-var PI = 4*Math.atan(1);
+var PI = 4 * Math.atan(1);
 var OpenLayers;
 var diagramColor = ["#000000","#660000","#ff0000","#ff9900","#ffff00","#66ff00","#66ffff","#ff00ff","#cccccc"];
 var traceColor = ["#414141","#FF5858","#59FF59","#59FFFF"];
@@ -145,81 +143,81 @@ var projmerc = new OpenLayers.Projection("EPSG:900913"); // projection according
 //var data;
 
 function toScient(x,dx) {
- dx = Math.round(Math.log(x/dx)/Math.log(10));
+ dx = Math.round(Math.log(x / dx) / Math.log(10));
  return parseFloat(x).toExponential(dx);
 }
 
 function invNKG(shower4326, stationIndex, data) {
- var S = $("#MIP"+stationIndex).val();
+ var S = $("#MIP" + stationIndex).val();
  var r;
  if (data.events[stationIndex].lat!=="") {
-  r = distVincenty(shower4326.y,shower4326.x,data.events[stationIndex].lat,data.events[stationIndex].lon);}
- return S*Math.pow((r/r0),(alfa))*Math.pow((1+(r/r0)),(eta-alfa));
+  r = distVincenty(shower4326.y, shower4326.x, data.events[stationIndex].lat, data.events[stationIndex].lon);}
+ return S * Math.pow((r / r0), (alfa)) * Math.pow((1 + (r / r0)), (eta - alfa));
 }
 
 function invAgase(shower4326, stationIndex, data) {
- var S = $("#MIP"+stationIndex).val();
+ var S = $("#MIP" + stationIndex).val();
  var r;
  if (data.events[stationIndex].lat!=="") {
-  r = distVincenty(shower4326.y,shower4326.x,data.events[stationIndex].lat,data.events[stationIndex].lon);}
- return S*Math.pow((r/r0), (-1.2))*Math.pow((1+(r/r0)), (-2.64))*Math.pow((1+r*r/1000000), (-0.6));
+  r = distVincenty(shower4326.y, shower4326.x, data.events[stationIndex].lat, data.events[stationIndex].lon);}
+ return S * Math.pow((r / r0), (-1.2)) * Math.pow((1 + (r / r0)), (-2.64)) * Math.pow((1 + r * r / 1000000), (-0.6));
 }
 
 function NKG(pMerc, k, stationIndex, data) {
  var p4326 = (pMerc);
- var r = distVincenty(p4326.y,p4326.x,data.events[i].lat,data.events[i].lon);
- return k*Math.pow((r/r0), (-alfa))*Math.pow((1+(r/r0)), (alfa-eta));
+ var r = distVincenty(p4326.y, p4326.x, data.events[i].lat,data.events[i].lon);
+ return k * Math.pow((r / r0), (-alfa)) * Math.pow((1 + (r / r0)), (alfa - eta));
 }
 
 function agase(pMerc, k, stationIndex, data) {
  var p4326 = (pMerc);
- var r = distVincenty(p4326.y,p4326.x,data.events[i].lat,data.events[i].lon);
- return k*Math.pow((r/r0),(1.2))*Math.pow((1+(r/r0)),(2.64))*Math.pow((1+r*r/1000000),(0.6));
+ var r = distVincenty(p4326.y, p4326.x, data.events[i].lat, data.events[i].lon);
+ return k * Math.pow((r / r0), (1.2)) * Math.pow((1 + (r / r0)), (2.64)) * Math.pow((1 + r * r / 1000000), (0.6));
 }
 
 function energy(k) {
- return 2.15e17*Math.pow((k*Math.pow((600/r0), (-alfa))*Math.pow((1+(600/r0)), (alfa-eta))), 1.015);
+ return 2.15e17 * Math.pow((k * Math.pow((600 / r0), (-alfa)) * Math.pow((1 + (600 / r0)), (alfa - eta))), 1.015);
 }
 
 function calcError(htmlInfo,data) {
  var chiKwad = 0;
  var delta;
  for (i=0; i<data.events.length; i++) {
-  delta = $("#"+htmlInfo.mipId+i).val()-$("#"+htmlInfo.mipCalcId+i).val();
-  chiKwad += delta*delta/$("#"+htmlInfo.mipCalcId+i).val();}
- $("#"+htmlInfo.stationEr).val(chiKwad.toFixed(4));
+  delta = $("#" + htmlInfo.mipId + i).val() - $("#" + htmlInfo.mipCalcId + i).val();
+  chiKwad += delta * delta / $("#" + htmlInfo.mipCalcId + i).val();}
+ $("#" + htmlInfo.stationEr).val(chiKwad.toFixed(4));
  result.error = chiKwad;
  for (j=0; j<4; j++) {
   for (i=0; i<data.events.length; i++) {
-   if ($("#"+htmlInfo.mipId+i+j).val()==="no data") {delta = 0;}
+   if ($("#" + htmlInfo.mipId + i + j).val()==="no data") {delta = 0;}
    else {delta = $("#" + htmlInfo.mipId + i + j).val();}
    delta = delta - $("#" + htmlInfo.mipCalcId + i).val();
    chiKwad += delta * delta / $("#" + htmlInfo.mipCalcId + i).val();}}
  $("#" + htmlInfo.showerEr).val(chiKwad.toFixed(4));
 }
 
-function calcEnergy(htmlInfo,showerMerc,data) {
- var k = invNKG(showerMerc,0,data);
+function calcEnergy(htmlInfo, showerMerc, data) {
+ var k = invNKG(showerMerc, 0, data);
  for (i=0; i<data.events.length; i++) {
-  $("#"+htmlInfo.mipCalcId+i).val(NKG(showerMerc,k,i,data).toFixed(3));
- }
- $("#"+htmlInfo.energyId).val(toScient(energy(k),(energy(k)/100)));
- result.logEnergy = Math.log(energy(k))/Math.log(10);
- calcError(htmlInfo,data);
+  $("#" + htmlInfo.mipCalcId + i).val(NKG(showerMerc, k, i, data).toFixed(3));}
+ $("#" + htmlInfo.energyId).val(toScient(energy(k), (energy(k)/100)));
+ result.logEnergy = Math.log(energy(k)) / Math.log(10);
+ calcError(htmlInfo, data);
 }
 
 function sendResult() {
- result.session_title=get_coincidence.session_title;
- result.session_pin=get_coincidence.session_pin;
- result.student_name=get_coincidence.student_name;
+ result.session_title = get_coincidence.session_title;
+ result.session_pin = get_coincidence.session_pin;
+ result.student_name = get_coincidence.student_name;
  $.getJSON('http://data.hisparc.nl/django/jsparc/result/', result, function(data) {
   $("#analyseTab").hide();
-  window.alert("You are number "+data.rank+".");
+  window.alert("You are number " + data.rank + ".");
+  // FIXME: This reload needs to be smarter and remember the Title and PIN (and probably also the Student name)
   window.location.reload();});
 }
 
 function transPlace(pMerc) {
- var p4326 = new OpenLayers.Geometry.Point(pMerc.geometry.x,pMerc.geometry.y); // makes a temporary helppoint
+ var p4326 = new OpenLayers.Geometry.Point(pMerc.geometry.x, pMerc.geometry.y); // makes a temporary helppoint
  p4326.transform(projmerc, proj4326); // transforms back to WGS 1984
  return p4326;
 }
@@ -228,21 +226,21 @@ function writeDist(htmlInfo,pMerc,data) {
  var p4326 = transPlace(pMerc);
  result.lon = p4326.x;
  result.lat = p4326.y;
- calcEnergy(htmlInfo,p4326,data);
+ calcEnergy(htmlInfo, p4326, data);
  for (i=0; i<data.events.length; i++) {
-  $("#"+htmlInfo.distId+i).val(distVincenty(p4326.y,p4326.x,data.events[i].lat,data.events[i].lon));}
+  $("#"+htmlInfo.distId+i).val(distVincenty(p4326.y, p4326.x, data.events[i].lat, data.events[i].lon));}
 }
 
 function makeShowerMap(htmlInfo,data) { //htmlInfo and data are JSON's!
  var mapData = {lon:0, lat:0, xmin:90, ymin:180, xmax:-90, ymax:-180};
  result.pk = data.pk;
  for (i=0; i<data.events.length; i++) {
-  mapData.lon=mapData.lon+data.events[i].lon;
-  mapData.lat=mapData.lat+data.events[i].lat;
-  if (mapData.xmax<data.events[i].lon) {mapData.xmax=data.events[i].lon;}
-  if (mapData.xmin>data.events[i].lon) {mapData.xmin=data.events[i].lon;}
-  if (mapData.ymax<data.events[i].lat) {mapData.ymax=data.events[i].lat;}
-  if (mapData.ymin>data.events[i].lat) {mapData.ymin=data.events[i].lat;}}
+  mapData.lon = mapData.lon+data.events[i].lon;
+  mapData.lat = mapData.lat+data.events[i].lat;
+  if (mapData.xmax<data.events[i].lon) {mapData.xmax = data.events[i].lon;}
+  if (mapData.xmin>data.events[i].lon) {mapData.xmin = data.events[i].lon;}
+  if (mapData.ymax<data.events[i].lat) {mapData.ymax = data.events[i].lat;}
+  if (mapData.ymin>data.events[i].lat) {mapData.ymin = data.events[i].lat;}}
  var x = mapData.lon / data.events.length;
  var y = mapData.lat / data.events.length;
  var lonlat = new OpenLayers.LonLat(x, y);
@@ -304,7 +302,7 @@ function plotGraph(htmlInfo,data) {
  for (j=0; j<data.events.length; j++) {
   for (k=0; k<4; k++) {
    while (maxHeight<data.events[j].pulseheights[k]) {
-    maxHeight=maxHeight*2;}}}
+    maxHeight = maxHeight*2;}}}
 
  for (j=0; j<data.events.length; j++) { // find the smallest and biggest value of nanoseconds
   offset = data.events[j].nanoseconds;
@@ -313,15 +311,15 @@ function plotGraph(htmlInfo,data) {
    if (tmax<data.events[j].traces[k].length*2.5+offset) {
     tmax = data.events[j].traces[k].length*2.5+offset;}}}
 
- var tracedata=[];
- var eventdata=[];
- var styledata={legend:{show:false},
-             title:"Coincidence",
-             cursor:{tooltipLocation:'sw', zoom:true, clickReset:true},
-             axes:{xaxis:{min:0, max:tmax-tmin, label:"Time [ns]", numberTicks:3},
-                   yaxis:{max:0, label:"Pulseheight [mV]", numberTicks:3,
-                          labelRenderer:$.jqplot.CanvasAxisLabelRenderer}},
-             series:[{showMarker:false, color:"#000000"}]
+ var tracedata = [];
+ var eventdata = [];
+ var styledata = {legend:{show:false},
+                  title:"Coincidence",
+                  cursor:{tooltipLocation:'sw', zoom:true, clickReset:true},
+                  axes:{xaxis:{min:0, max:tmax-tmin, label:"Time [ns]", numberTicks:3},
+                        yaxis:{max:0, label:"Pulseheight [mV]", numberTicks:3,
+                        labelRenderer:$.jqplot.CanvasAxisLabelRenderer}},
+                  series:[{showMarker:false, color:"#000000"}]
  };
 
  for (j=0;j<data.events.length;j++) {
@@ -386,5 +384,5 @@ function plotGraph(htmlInfo,data) {
   for (k=0; k<4; k++) {
    styledata.series[k].color=traceColor[k];}
 
- $.jqplot(diagramID, eventdata, styledata);}
+  $.jqplot(diagramID, eventdata, styledata);}
 }
