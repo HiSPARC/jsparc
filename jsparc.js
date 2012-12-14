d25c7277dea04eca8d9310429b58df2f2bc2e204
Arne de Laat <adelaat@nikhef.nl>
2012-12-14 11:12:06 +0100

    Add get example coincidence
    Pressing the Get example button gives back a random coincidence
    The returned results will not be stored
    This is similar to using the 'Test student' for a specific session

--- jsparc.js
+++ jsparc.js
@@ -242,7 +242,7 @@ function sendResult() {
     result.student_name = get_coincidence.student_name;
     $.getJSON(URL + 'result/', result, function(data) {
         $("#analyseTab").hide();
-        window.alert("You are number " + data.rank + ".");
+        window.alert(data.msg + "\nYou are number " + data.rank + ".");
         // FIXME: This 'reload' should be smarter and remember the Title and PIN (possible probably also the Student name)
         window.location.reload();
     });
