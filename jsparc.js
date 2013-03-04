5596b937a55472c4f56552cd38079c1dad250e56
Niek Schultheiss <nieks@nikhef.nl>
2013-03-04 16:38:15 +0100

    Standard deviation for times added, new chiSquared function.

--- jsparc.js
+++ jsparc.js
@@ -71,45 +71,29 @@
  * @returns (Number} distance in metres between points
  */
 
-var c = 299792458;
-var toRad=Math.atan(1)/45;
+var c = 299792458, toRad=Math.atan(1)/45;
 
 
 function distVincenty(lat1, lon1, lat2, lon2) {
-    var a = 6378137,
-        b = 6356752.314245,
-        f = 1 / 298.257223563; // WGS-84 ellipsoid params
+    var a = 6378137, b = 6356752.314245, f = 1 / 298.257223563; // WGS-84 ellipsoid params
     var L = toRad*(lon2 - lon1);
-    var U1 = Math.atan((1 - f) * Math.tan(toRad*lat1));
-    var U2 = Math.atan((1 - f) * Math.tan(toRad*lat2));
-    var sinU1 = Math.sin(U1),
-        cosU1 = Math.cos(U1);
-    var sinU2 = Math.sin(U2),
-        cosU2 = Math.cos(U2);
-    var sinSigma;
-    var cosSigma;
-    var sigma;
-    var cosSqAlpha;
-    var cos2SigmaM;
+    var U1 = Math.atan((1 - f) * Math.tan(toRad*lat1)), U2 = Math.atan((1 - f) * Math.tan(toRad*lat2));
+    var sinU1 = Math.sin(U1), cosU1 = Math.cos(U1);
+    var sinU2 = Math.sin(U2), cosU2 = Math.cos(U2);
+    var sinSigma, cosSigma, sigma, cosSqAlpha, cos2SigmaM;
 
-    var lambda = L,
-        lambdaP, iterLimit = 100;
+    var lambda = L, lambdaP, iterLimit = 100;
 
     do {
-        var sinLambda = Math.sin(lambda),
-            cosLambda = Math.cos(lambda);
+        var sinLambda = Math.sin(lambda), cosLambda = Math.cos(lambda);
         sinSigma = Math.sqrt((cosU2 * sinLambda) * (cosU2 * sinLambda) + (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) * (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda));
-        if (sinSigma === 0) {
-            return 0;
-        } // co-incident points
+        if (sinSigma === 0) return 0; // co-incident points
         cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda;
         sigma = Math.atan2(sinSigma, cosSigma);
         var sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
         cosSqAlpha = 1 - sinAlpha * sinAlpha;
         cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha;
-        if (isNaN(cos2SigmaM)) {
-            cos2SigmaM = 0;
-        } // equatorial line: cosSqAlpha=0 (§6)
+        if (isNaN(cos2SigmaM)) cos2SigmaM = 0; // equatorial line: cosSqAlpha=0 (§6)
         var C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
         lambdaP = lambda;
         lambda = L + (1 - C) * f * sinAlpha * (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM)));
@@ -119,8 +103,7 @@ function distVincenty(lat1, lon1, lat2, lon2) {
         return NaN;} // formula failed to converge
 
     var uSq = cosSqAlpha * (a * a - b * b) / (b * b);
-    var A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
-    var B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
+    var A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq))), B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
     var deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM)));
     var s = b * A * (sigma - deltaSigma);
 
@@ -137,12 +120,7 @@ function distVincenty(lat1, lon1, lat2, lon2) {
 /* End of Vincenty Inverse Solution of Geodesics on the Ellipsoid (c) Chris Veness 2002-2010       */
 /* Code clean up by N.G.Schultheiss using http://jslint.com/                                       */
 
-var showerMerc, shower4326;
-var PI = 4 * Math.atan(1);
-var OpenLayers;
-var alfa = 1.2;
-var eta = 3.97; //3,97 – 1,79⋅((1/cos θ) – 1)
-var r0 = 92;
+var showerMerc, shower4326, PI = 4 * Math.atan(1), OpenLayers, r0 = 92, alfa = 1.2, eta = 3.97; //3,97 – 1,79⋅((1/cos θ) – 1)
 var result = {};
 var proj4326 = new OpenLayers.Projection("EPSG:4326"); // projection according WGS 1984
 var projmerc = new OpenLayers.Projection("EPSG:900913"); // projection according Mercator
@@ -170,31 +148,27 @@ function toScient(x, dx) {
     return parseFloat(x).toExponential(dx);
 }
 
-function invNKG(shower4326, stationIndex, data) {
-    var S = $("#MIP" + stationIndex).val();
-    var r;
+function invNKG(shower4326, stationIndex, data, htmlInfo) {
+    var S = $("#" + htmlInfo.mipId + stationIndex).val(), r;
     if (data.events[stationIndex].lat !== "") {
         r = distVincenty(shower4326.y, shower4326.x, data.events[stationIndex].lat, data.events[stationIndex].lon);}
     return S * Math.pow((r / r0), (alfa)) * Math.pow((1 + (r / r0)), (eta - alfa));
 }
 
-function invAgase(shower4326, stationIndex, data) {
-    var S = $("#MIP" + stationIndex).val();
-    var r;
+function invAgase(shower4326, stationIndex, data, htmlInfo) {
+    var S = $("#" + htmlInfo.mipId+ stationIndex).val(), r;
     if (data.events[stationIndex].lat !== "") {
         r = distVincenty(shower4326.y, shower4326.x, data.events[stationIndex].lat, data.events[stationIndex].lon);}
     return S * Math.pow((r / r0), (-1.2)) * Math.pow((1 + (r / r0)), (-2.64)) * Math.pow((1 + r * r / 1000000), (-0.6));
 }
 
 function NKG(pMerc, k, stationIndex, data) {
-    var p4326 = (pMerc);
-    var r = distVincenty(p4326.y, p4326.x, data.events[stationIndex].lat, data.events[stationIndex].lon);
+    var p4326 = (pMerc), r = distVincenty(p4326.y, p4326.x, data.events[stationIndex].lat, data.events[stationIndex].lon);
     return k * Math.pow((r / r0), (-alfa)) * Math.pow((1 + (r / r0)), (alfa - eta));
 }
 
 function agase(pMerc, k, stationIndex, data) {
-    var p4326 = (pMerc);
-    var r = distVincenty(p4326.y, p4326.x, data.events[i].lat, data.events[i].lon);
+    var p4326 = (pMerc), r = distVincenty(p4326.y, p4326.x, data.events[i].lat, data.events[i].lon);
     return k * Math.pow((r / r0), (1.2)) * Math.pow((1 + (r / r0)), (2.64)) * Math.pow((1 + r * r / 1000000), (0.6));
 }
 
@@ -202,27 +176,48 @@ function energy(k) {
     return 2.15e17 * Math.pow((k * Math.pow((600 / r0), (-alfa)) * Math.pow((1 + (600 / r0)), (alfa - eta))), 1.015);
 }
 
+function calcDev(measurement, calculation){
+    var deviation=0;
+    var delta
+    for (i = 0; i < measurement.length; i++) {
+        delta = measurement[i]-calculation[i];
+        deviation += delta * delta;
+    }
+    return Math.sqrt(deviation / measurement.length);
+}
+
+function calcChiSq(measurement, calculation){
+    var chiSq = 0, delta;
+    for (i = 0; i < measurement.length; i++) {
+        if(measurement[i] != 0){
+        delta = measurement[i]-calculation[i];
+        chiSq += delta * delta / measurement[i];}
+    }
+    return chiSq;
+}
+
 function calcError(htmlInfo, data) {
-    var chiKwad = 0;
-    var delta;
+    var chiSq = 0, delta, measurement = new Array, calculation = new Array;
     for (i = 0; i < data.events.length; i++) {
-        delta = $("#" + htmlInfo.mipId + i).val() - $("#" + htmlInfo.mipCalcId + i).val();
-        chiKwad += delta * delta / $("#" + htmlInfo.mipCalcId + i).val();}
-    $("#" + htmlInfo.stationEr).val(chiKwad.toFixed(4));
-    result.error = chiKwad;
-    for (j = 0; j < 4; j++) {
+        measurement[i] = $("#" + htmlInfo.mipId + i).val();
+        calculation[i] = $("#" + htmlInfo.mipCalcId + i).val();
+    }
+    chiSq = calcChiSq(measurement, calculation);
+    $("#" + htmlInfo.stationEr).val(chiSq.toFixed(4));
+    result.error = chiSq;
+/*    for (j = 0; j < 4; j++) {
         for (i = 0; i < data.events.length; i++) {
             if ($("#" + htmlInfo.mipId + i + j).val() === "no data") {
                 delta = 0;}
             else {
                 delta = $("#" + htmlInfo.mipId + i + j).val();}
             delta = delta - $("#" + htmlInfo.mipCalcId + i).val();
-            chiKwad += delta * delta / $("#" + htmlInfo.mipCalcId + i).val();}}
-    $("#" + htmlInfo.showerEr).val(chiKwad.toFixed(4));
+            chiSq += delta * delta / $("#" + htmlInfo.mipCalcId + i).val();}}
+    $("#" + htmlInfo.showerEr).val(chiSq.toFixed(4));*/
 }
 
 function calcEnergy(htmlInfo, showerMerc, data) {
-    var k = invNKG(showerMerc, 0, data);
+    var k = invNKG(showerMerc, 0, data, htmlInfo), mipData = new Array();
     for (i = 0; i < data.events.length; i++) {
         $("#" + htmlInfo.mipCalcId + i).val(NKG(showerMerc, k, i, data).toFixed(3));}
     $("#" + htmlInfo.energyId).val(toScient(energy(k), (energy(k) / 100)));
@@ -259,13 +254,7 @@ function writeDist(htmlInfo, pMerc, data) {
 
 
 function makeShowerMap(htmlInfo, data) { //htmlInfo and data are JSON's!
-    var mapData = {
-        lon: 0,
-        lat: 0,
-        xmin: 90,
-        ymin: 180,
-        xmax: -90,
-        ymax: -180};
+    var mapData = {lon: 0, lat: 0, xmin: 90, ymin: 180, xmax: -90, ymax: -180};
     result.pk = data.pk;
     for (i = 0; i < data.events.length; i++) {
         mapData.lon += data.events[i].lon;
@@ -282,8 +271,7 @@ function makeShowerMap(htmlInfo, data) { //htmlInfo and data are JSON's!
                     {dragPanOptions: {enableKinetic: true}}),
             new OpenLayers.Control.Attribution(),
             new OpenLayers.Control.ScaleLine()]};
-    var map = new OpenLayers.Map(htmlInfo.mapId, options);
-    var mapLayer = new OpenLayers.Layer.OSM();
+    var map = new OpenLayers.Map(htmlInfo.mapId, options), mapLayer = new OpenLayers.Layer.OSM();
     map.addLayer(mapLayer);
     map.setCenter(new OpenLayers.LonLat(4.950, 52.355).transform(proj4326, projmerc), 5); //shows the map
 
@@ -329,10 +317,7 @@ function makeShowerMap(htmlInfo, data) { //htmlInfo and data are JSON's!
 }
 
 function plotGraph(htmlInfo, data) {
-    var tmin = 999999999;
-    var tmax = 0;
-    var diagramID;
-    var offset;
+    var tmin = 999999999, tmax = 0, diagramID, offset;
     var plotStyle = {
         seriesDefaults: {
             lineWidth: 1.5,
@@ -388,9 +373,7 @@ function plotGraph(htmlInfo, data) {
         if (tmax < data.events[j].traces[0].length * 2.5 + offset) {
             tmax = data.events[j].traces[0].length * 2.5 + offset;}}
 
-    var tracedata = [];
-    var eventdata = [];
-    var trace_min = 0;
+    var tracedata = [], eventdata = [], trace_min = 0;
 
     for (j = 0; j < data.events.length; j++) {
         for (k = 0; k < detNum[j]; k++) {
@@ -459,14 +442,9 @@ function plotGraph(htmlInfo, data) {
 }
 
 function toZenith(RA,Dec,Lon,Lat,ST){
-    var sinDec = Math.sin(Dec * toRad);
-    var cosDec = Math.cos(Dec * toRad);
-    var sinLat = Math.sin(Lat * toRad);
-    var cosLat = Math.cos(Lat * toRad);
-    var ha = ST - Lon - RA;
-    var cosZenith = (sinDec * sinLat + cosDec * cosLat * Math.cos(ha * toRad)).toFixed(4);
-    var zenith = Math.acos(cosZenith) / toRad;
-    var azimuth;
+    var sinDec = Math.sin(Dec * toRad), cosDec = Math.cos(Dec * toRad), sinLat = Math.sin(Lat * toRad), cosLat = Math.cos(Lat * toRad);
+    var ha = ST - Lon - RA, cosZenith = (sinDec * sinLat + cosDec * cosLat * Math.cos(ha * toRad)).toFixed(4);
+    var zenith = Math.acos(cosZenith) / toRad, azimuth;
     if (cosLat * Math.sin(zenith * toRad).toFixed(4) == 0) {azimuth = 0;}
     else {azimuth = (Math.acos((sinDec - sinLat * cosZenith) / (cosLat * Math.sin(zenith * toRad)))).toFixed(4) / toRad;}
     if (Math.sin(ha*toRad) > 0) {azimuth = 360 - azimuth;}
@@ -475,24 +453,17 @@ function toZenith(RA,Dec,Lon,Lat,ST){
 }
 
 function zenithData(data, star) {
-    var x, y, r;
-    var mapData = {
-        lon: 0,
-        lat: 0};
+    var x, y, r, mapData = {lon: 0, lat: 0};
     for (i = 0; i < data.events.length; i++) {
         mapData.lon += data.events[i].lon;
         mapData.lat += data.events[i].lat;}
-    var Lon = mapData.lon / data.events.length;
-    var Lat = mapData.lat / data.events.length;
-    var ST = 0;
+    var Lon = mapData.lon / data.events.length, Lat = mapData.lat / data.events.length, ST = 0;
     for (i = 0; i < star.length; i++){
        for(j = 0; j < star[i].length; j++){
            var netCoordInput = toZenith(star[i][j][0], star[i][j][1], Lon, Lat, (data.events[0].timestamp / 86400 - 10957));
            star[i][j][1] = netCoordInput.zenith;
            star[i][j][0] = netCoordInput.azimuth;
-           if(star[i][j][1] > 45){
-               star[i].splice(j, 1);
-               j--;}
+           if(star[i][j][1] > 45) {star[i].splice(j, 1); j--;}
            else {
                r = star[i][j][1];
                x = r * Math.sin(toRad * star[i][j][0]);
@@ -561,14 +532,10 @@ function makeStarMap(htmlInfo, star) {
 }
 
 function WGS84toECEF(lat, lon, alt){
-    var a=6378137;
-    var b=6356752.315;
+    var a=6378137, b=6356752.315;
     lon=toRad*lon;
     lat=toRad*lat;
-    var x=(a+alt)*Math.cos(lat)*Math.cos(lon);
-    var y=(a+alt)*Math.cos(lat)*Math.sin(lon);
-    var z=(b+alt)*Math.sin(lat);
-    var coordinate={"x":x,"y":y,"z":z,"lat":lat};
+    var x=(a+alt)*Math.cos(lat)*Math.cos(lon), y=(a+alt)*Math.cos(lat)*Math.sin(lon), z=(b+alt)*Math.sin(lat), coordinate={"x":x,"y":y,"z":z,"lat":lat};
     return coordinate;
 }
 
@@ -581,14 +548,11 @@ function netLoc(data, height){
 }
 
 function showerDirection(data){
-    var showerDir = {};
-    var norm;
- //   var latitude;
+    var showerDir = {}, norm, netCoord = netLoc(data, 20000);
     showerDir.x = 0;
     showerDir.y = 0;
     showerDir.z = 0;
     showerDir.lat = 0;
-    var netCoord = netLoc(data, 20000);
     for(var i = 0; i< data.events.length; i++){
         showerDir.x += eval(netCoord[i].x);
         showerDir.y += eval(netCoord[i].y);
@@ -600,8 +564,8 @@ function showerDirection(data){
     showerDir.z = showerDir.z / data.events.length;
     showerDir.lat = showerDir.lat / data.events.length;
     norm = 1/(Math.sqrt(showerDir.x*showerDir.x+showerDir.y*showerDir.y))
-    showerDir.xLon = showerDir.y*norm;
-    showerDir.yLon = -showerDir.x*norm;
+    showerDir.xLon = -showerDir.y*norm;
+    showerDir.yLon = showerDir.x*norm;
     showerDir.zLat = Math.cos(showerDir.lat);
     showerDir.yLat = -showerDir.y*norm*Math.sin(showerDir.lat);
     showerDir.xLat = -showerDir.x*norm*Math.sin(showerDir.lat);
@@ -609,26 +573,19 @@ function showerDirection(data){
 }
 
 function timeCalc(htmlInfo, data, dRA, dDec){ 
-    var netCoord = netLoc(data, 0);
-    var showerDir = showerDirection(data);
+    var measurement = new Array, calculation = new Array, netCoord = netLoc(data, 0), showerDir = showerDirection(data);
     var z = (showerDir.z+20000*showerDir.zLat*Math.tan(dDec*toRad)).toFixed(0);
-    var y = (showerDir.y+20000*(showerDir.yLat*Math.tan(dDec*toRad)-showerDir.xLon*Math.tan(dRA*toRad))).toFixed(0);
-    var x = (showerDir.x+20000*(showerDir.xLat*Math.tan(dDec*toRad)-showerDir.yLon*Math.tan(dRA*toRad))).toFixed(0);
-    var dx = x-netCoord[0].x;
-    var dy = y-netCoord[0].y;
-    var dz = z-netCoord[0].z;
-    var time = Math.sqrt(dx*dx+dy*dy+dz*dz)/c;
- //   $('#nanoCalc0').val(0);
-    for(var i=0; i<netCoord.length; i++){
-        dx = x-netCoord[i].x;
-        dy = y-netCoord[i].y;
-        dz = z-netCoord[i].z;
-        dtime = 1000000000*(Math.sqrt(dx*dx+dy*dy+dz*dz)/c-time);
-        $('#nanoCalc' + i).val(dtime.toFixed(0));
+    var y = (showerDir.y+20000*(showerDir.yLat*Math.tan(dDec*toRad)+showerDir.xLon*Math.tan(dRA*toRad))).toFixed(0);
+    var x = (showerDir.x+20000*(showerDir.xLat*Math.tan(dDec*toRad)+showerDir.yLon*Math.tan(dRA*toRad))).toFixed(0);
+    var dx = x-netCoord[0].x, dy = y-netCoord[0].y, dz = z-netCoord[0].z, time = Math.sqrt(dx*dx+dy*dy+dz*dz)/c;
+    for(var i = 0; i < netCoord.length; i++){
+        dx = x-netCoord[i].x; dy = y-netCoord[i].y; dz = z-netCoord[i].z;
+        measurement[i] = data.events[i].nanoseconds-data.events[0].nanoseconds;
+        calculation[i] = 1e9*(Math.sqrt(dx*dx+dy*dy+dz*dz)/c-time).toFixed(9);
+        $('#nanoCalc' + i).val((calculation[i]).toFixed(0));
     }
-    $('#dirEr').val(x);
-    $('#RA').val(y);
-    $('#Dec').val(z);
+    var chiSq = calcDev(measurement, calculation);
+    $('#dirEr').val(chiSq.toFixed(0));
+    $('#RA').val(calculation[2]);
+    $('#Dec').val(calculation[1]);
 }
-
-
