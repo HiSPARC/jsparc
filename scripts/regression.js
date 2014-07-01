0d5612944916b6f64b5127c7e91ce2859bcdcf83
Arne de Laat <adelaat@nikhef.nl>
2014-07-01 12:38:26 +0200

    Update code styling in regression

--- scripts/regression.js
+++ scripts/regression.js
@@ -3,7 +3,7 @@
 *
 * Regression.JS - Regression functions for javascript
 * http://tom-alexander.github.com/regression-js/
-* 
+*
 * copyright(c) 2013 Tom Alexander
 * Licensed under the MIT license.
 *
@@ -67,14 +67,14 @@
             var denominator = (n * sum[2] - sum[0] * sum[0]);
             var A = (sum[1] * sum[2] - sum[0] * sum[3]) / denominator;
             var B = (n * sum[3] - sum[0] * sum[1]) / denominator;
-            
+
             var ssxx = sum[2] - sum[0] * sum[0] / n;
             var ssxy = sum[3] - sum[0] * sum[1] / n;
             var ssyy = sum[4] - sum[1] * sum[1] / n;
 
             for (var i = 0, len = data.length; i < len; i++) {
                 x = data[i][0];
-                var yf = A + B * x;         
+                var yf = A + B * x;
                 var coordinate = [x, yf];
                 results.push(coordinate);}
 
@@ -88,47 +88,44 @@
 
 
         gaussian: function(data) {
-        
+
             // Gaussian regression. From the relative cumulative distribution Y the z value is obtained as
             // the real root of the cubic z^3 + (1.5976/0.07056)z - (1/0.07056)ln (Y/(1-Y))
             // The relation between Y and z is from Bowling et al., JIEM, p 114-127 (2009).
             // To fit z with the linear relation A + Bx the function Sum((A + Bx - z)^2) is mimimized for the range -1.4 < z < 1.4.
-            // Correlation coefficient is calculated according to r^2 = 1 - SSE/SST, where SSE is the sum of the squared deviations of y-data 
+            // Correlation coefficient is calculated according to r^2 = 1 - SSE/SST, where SSE is the sum of the squared deviations of y-data
             // with respect to y-regression and where SST is the sum of the deviations of y-data with respect to the mean of y-data.
 
             var n = 0;
             var sumdata = [];
             for (n = 0; n < data.length; n++) {
-                    sumdata.push([data[n][0], data[n][1]]);}
+                sumdata.push([data[n][0], data[n][1]]);}
 
-            n = 0;
-            for (; n < sumdata.length - 1; n++) {
-                    sumdata[n+1][1] = data[n+1][1] + sumdata[n][1];}
-                    
+            for (n = 0; n < sumdata.length - 1; n++) {
+                sumdata[n+1][1] = data[n+1][1] + sumdata[n][1];}
+
             var max = sumdata[sumdata.length - 1][1];
             var width = sumdata[sumdata.length - 1][0] - sumdata[0][0];
             var reldata = sumdata;
-            n = 0;
-            for (; n < reldata.length ; n++) {
-                    reldata[n][1] = sumdata[n][1] / max;}
-            
+            for (n = 0; n < reldata.length ; n++) {
+                reldata[n][1] = sumdata[n][1] / max;}
+
             var sum = [0, 0, 0, 0, 0, 0], n = 0, x = 0, y = 0, results = [];
             var p = 22.64172356, q = 0, psi = 0, root2 = 0, root3 = 0, z = 0;
-            n = 0;
-            for (; n < reldata.length ; n++) {
-                    psi = reldata[n][1];
-                    q = -14.1723356 * Math.log(psi/(1 - psi));
-                    root2 = Math.sqrt(q * q / 4 + p * p * p / 27);
-                    root3 = Math.pow((root2 - q / 2), 1 / 3);
-                    z = root3 - p / (3 * root3);
-                    if (z < 1.4 && z > -1.4) {
-                        x = reldata[n][0];
-                        sum[0] += x;
-                        sum[1] += z;
-                        sum[2] += x * x;
-                        sum[3] += x * z;
-                        sum[4] += z * z;
-                        sum[5] += 1;}}
+            for (n = 0; n < reldata.length ; n++) {
+                psi = reldata[n][1];
+                q = -14.1723356 * Math.log(psi/(1 - psi));
+                root2 = Math.sqrt(q * q / 4 + p * p * p / 27);
+                root3 = Math.pow((root2 - q / 2), 1 / 3);
+                z = root3 - p / (3 * root3);
+                if (z < 1.4 && z > -1.4) {
+                    x = reldata[n][0];
+                    sum[0] += x;
+                    sum[1] += z;
+                    sum[2] += x * x;
+                    sum[3] += x * z;
+                    sum[4] += z * z;
+                    sum[5] += 1;}}
 
             n = sum[5];
             var denominator = (n * sum[2] - sum[0] * sum[0]);
@@ -144,11 +141,11 @@
                 var yf = norm * Math.exp(-0.5 * (A + B * x) * (A + B * x));
                 var yg = max / data.length
                 y = data[i][1];
-                SSE += (y-yf) * (y-yf); 
-                SST += (y-yg) * (y-yg);           
+                SSE += (y - yf) * (y - yf);
+                SST += (y - yg) * (y - yg);
                 var coordinate = [x, yf];
                 results.push(coordinate);}
-                
+
             var corr = Math.sqrt(1 - SSE / SST) * Math.sqrt(1 - SSE / SST);
             var corrstring = 'r^2 = ' + corr.toFixed(3);
             var string = 'y = ' + norm.toExponential(2) +'\\cdot e^{-\\frac{1}{2} \\left( \\frac{x - ' + mu.toExponential(2) + '}{'+ sigma.toExponential(2) +'}\\right)^2}';
@@ -181,14 +178,14 @@
             var denominator = (n * sum[2] - sum[0] * sum[0]);
             var A = Math.exp((sum[1] * sum[2] - sum[0] * sum[3]) / denominator);
             var B = (n * sum[3] - sum[0] * sum[1]) / denominator;
-            
+
             var ssxx = sum[2] - sum[0] * sum[0] / n;
             var ssxy = sum[3] - sum[0] * sum[1] / n;
             var ssyy = sum[4] - sum[1] * sum[1] / n;
 
             for (var i = 0, len = data.length; i < len; i++) {
                 x = data[i][0];
-                var yf = A * Math.exp(B * x);         
+                var yf = A * Math.exp(B * x);
                 var coordinate = [x, yf];
                 results.push(coordinate);}
 
@@ -206,21 +203,21 @@
 
             // Exponential regression with equally weights, see http://mathworld.wolfram.com/LeastSquaresFittingExponential.html
             // To fit y = A exp(Bx) --> ln y = ln A + Bx  the function Sum(y(ln A + Bx - ln y)^2) is mimimized.
-            // Correlation coefficient is calculated according to r^2 = 1 - SSE/SST, where SSE is the sum of the squared deviations of y-data 
+            // Correlation coefficient is calculated according to r^2 = 1 - SSE/SST, where SSE is the sum of the squared deviations of y-data
             // with respect to y-regression and where SST is the sum of the deviations of y-data with respect to the mean of y-data.
 
             for (len = data.length; n < len; n++) {
                 if (data[n][1] > 0) {
                     x = data[n][0];
                     y = data[n][1];
-                    lny = Math.log(y);    
+                    lny = Math.log(y);
                     sum[0] += x;
                     sum[1] += y;
                     sum[2] += x * x * y;
                     sum[3] += y * lny;
                     sum[4] += x * y * lny;
                     sum[5] += x * y;}}
- 
+
             var denominator = (sum[1] * sum[2] - sum[5] * sum[5]);
             var A = Math.exp((sum[2] * sum[3] - sum[5] * sum[4]) / denominator);
             var B = (sum[1] * sum[4] - sum[5] * sum[3]) / denominator;
@@ -228,7 +225,7 @@
             sum = [0, 0, 0, 0, 0, 0];
             n = 0;
             y = 0;
-            var SSE = 0, sy = 0, syy = 0; 
+            var SSE = 0, sy = 0, syy = 0;
             for (var i = 0, len = data.length; i < len; i++) {
                 if (data[i][1] > 0) {
                     x = data[i][0];
@@ -237,7 +234,7 @@
                     syy += y * y;
                     sum[5] += 1;
                     var yf = A * Math.exp(B * x);
-                    SSE += (y-yf) * (y-yf);           
+                    SSE += (y-yf) * (y-yf);
                     var coordinate = [x, yf];
                     results.push(coordinate);}}
 
@@ -273,7 +270,7 @@
             var denominator = (n * sum[2] - sum[0] * sum[0]);
             var A = (sum[1] * sum[2] - sum[0] * sum[3]) / denominator;
             var B = (n * sum[3] - sum[0] * sum[1]) / denominator;
-            
+
             var ssxx = sum[2] - sum[0] * sum[0] / n;
             var ssxy = sum[3] - sum[0] * sum[1] / n;
             var ssyy = sum[4] - sum[1] * sum[1] / n;
@@ -281,7 +278,7 @@
             for (var i = 0, len = data.length; i < len; i++) {
                 x = data[i][0];
                 if (x > 0) {
-                    var yf = A + B * Math.log(x);         
+                    var yf = A + B * Math.log(x);
                     var coordinate = [x, yf];
                     results.push(coordinate);}}
 
@@ -316,14 +313,14 @@
             var denominator = (n * sum[2] - sum[0] * sum[0]);
             var A = Math.exp((sum[1] * sum[2] - sum[0] * sum[3]) / denominator);
             var B = (n * sum[3] - sum[0] * sum[1]) / denominator;
-            
+
             var ssxx = sum[2] - sum[0] * sum[0] / n;
             var ssxy = sum[3] - sum[0] * sum[1] / n;
             var ssyy = sum[4] - sum[1] * sum[1] / n;
 
             for (var i = 0, len = data.length; i < len; i++) {
                 x = data[i][0];
-                var yf = A * Math.pow(x, B);         
+                var yf = A * Math.pow(x, B);
                 var coordinate = [x, yf];
                 results.push(coordinate);}
 
@@ -336,11 +333,11 @@
 
 
         polynomial: function(data, order) {
-        
+
             // Non-linear regression, see http://mathworld.wolfram.com/LeastSquaresFittingPolynomial.html
-            // Correlation coefficient is calculated according to r^2 = 1 - SSE/SST, where SSE is the sum of the squared deviations of y-data 
+            // Correlation coefficient is calculated according to r^2 = 1 - SSE/SST, where SSE is the sum of the squared deviations of y-data
             // with respect to y-regression and where SST is the sum of the deviations of y-data with respect to the mean of y-data.
-        
+
             if (typeof order == 'undefined') {
                 order = 2;
             }
@@ -371,13 +368,13 @@
                 var answer = 0;
                 for (var w = 0; w < equation.length; w++) {
                     answer += equation[w] * Math.pow(data[i][0], w);}
-                    
+
                 y = data[i][1];
                 sy += y;
                 syy += y * y;
                 n += 1;
                 SSE += (y-answer) * (y-answer);
-                
+
                 results.push([data[i][0], answer]);}
 
             var SST = syy - sy * sy / n;
@@ -402,7 +399,7 @@
                     results.push([data[i][0], data[i][1]]);}
                 else {
                     results.push([data[i][0], lastvalue]);}}
-  
+
             return {equation: [lastvalue], points: results, string: "" + lastvalue};
         }
     };
