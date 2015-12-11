62ec8d6b38c5afbc12626a19af4499a478821e03
Arne de Laat <adelaat@nikhef.nl>
2015-12-11 02:00:31 +0100

    Download coincidences for all subclusters and network
    Add dropdown menu to event-display to choose subcluster.
    Reload page when switching dataset.
    Reduce data size by rounding particle counts,
    removing timestamp and nanoseconds,
    and removing indentation from json dump.
    Bandwidth saved may be negligible if the server gzips data anyway.
    Change date to date with most simultaneous active stations.

--- event-display/create_test_json.py
+++ event-display/create_test_json.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import re
+import os
 
 import tables
 
@@ -8,38 +9,57 @@ from sapphire import Network, Station, download_data, download_coincidences
 from sapphire.transformations.clock import datetime_to_gps
 
 
-# Excluded 510 because it overlaps with 501
-STATIONS = [501, 502, 503, 504, 505, 506, 508, 509, 511]
-START = datetime.datetime(2015, 9, 5, 10)
-END = datetime.datetime(2015, 9, 5, 11)
+STATIONS = Network().station_numbers()
+START = datetime.datetime(2013, 6, 30, 14, 0)
+END = datetime.datetime(2013, 6, 30, 14, 20)
 LIMITS = [datetime_to_gps(START) * int(1e9),
           datetime_to_gps(END) * int(1e9)]
-
 
 re_station_number = re.compile(".*/station_([0-9]+)$")
 
 
-def download_data_coincidences(data):
-    if '/coincidences' not in data:
-        download_coincidences(data, stations=STATIONS, start=START, end=END)
+def download_coincidences_data(data):
+    for subcluster in Network().subclusters():
+        group = ('/coincidences_%s' %
+                 subcluster['name'].lower().replace(' ', '_'))
+        if group in data:
+            continue
+        stations = Network().station_numbers(subcluster=subcluster['number'])
+        if len(stations) < 2:
+            continue
+        download_coincidences(data, group=group, stations=stations,
+                              start=START, end=END)
 
+    # Entire network
+    if '/coincidences' not in data:
+        download_coincidences(data, start=START, end=END)
+
+
+def download_events_data(data):
     for station in STATIONS:
         group = '/s%d' % station
         if group not in data:
             download_data(data, group, station, start=START, end=END)
 
 
-def build_coincidence_json(data):
+def build_coincidence_json(data, subcluster=None):
     vis_coincidences = []
 
-    coincidences = data.root.coincidences.coincidences
-    c_index = data.root.coincidences.c_index
-    s_index = data.root.coincidences.s_index
+    if subcluster is None:
+        group = '/coincidences/'
+    else:
+        group = '/coincidences_%s/coincidences/' % subcluster
+
+    try:
+        coincidences = data.get_node(group, 'coincidences')
+        c_index = data.get_node(group, 'c_index')
+        s_index = data.get_node(group, 's_index')
+    except tables.NoSuchNodeError:
+        return []
 
     for coincidence in coincidences:
-        vis_coincidence = {'timestamp': coincidence['timestamp'],
-                           'nanoseconds': coincidence['nanoseconds'],
-                           'ext_timestamp': coincidence['ext_timestamp']}
+        vis_coincidence = {u: coincidence[u] for u in
+                           ['timestamp', 'nanoseconds', 'ext_timestamp']}
         events = []
         for s_idx, e_idx in c_index[coincidence['id']]:
             node_str = s_index[s_idx]
@@ -47,9 +67,9 @@ def build_coincidence_json(data):
             station_event = station.events[e_idx]
             station_number = re_station_number.match(node_str).group(1)
 
-            event = {u: float(station_event[u])
-                     if station_event[u] >= 0. else 0. for u in
-                     ['n1', 'n2', 'n3', 'n4']}
+            event = {u: round(float(station_event[u]), 2)
+                     if station_event[u] >= 0. else 0.
+                     for u in ['n1', 'n2', 'n3', 'n4']}
             event['station'] = station_number
             events.append(event)
         vis_coincidence['events'] = events
@@ -64,18 +84,23 @@ def build_events_json(data, station):
     events = data.get_node('/s%d/events' % station)
 
     for event in events:
-        output_event = {u: event[u] for u in ['timestamp', 'nanoseconds',
+        output_event = {u: event[u] for u in [#'timestamp', 'nanoseconds',
                                               'ext_timestamp']}
-        output_event.update({u: event[u] if event[u] >= 0. else 0. for u
-                             in ['n1', 'n2', 'n3', 'n4']})
+        output_event.update({u: round(float(event[u]), 2)
+                             if event[u] >= 0. else 0.
+                             for u in ['n1', 'n2', 'n3', 'n4']})
         output.append(output_event)
 
     return output
 
 
-def build_station_json(data):
+def build_station_json(data, subcluster=None):
     stations = {}
-    for station_number in STATIONS:
+    if subcluster is None:
+        station_numbers = STATIONS
+    else:
+        station_numbers = Network().station_numbers(subcluster=subcluster)
+    for station_number in station_numbers:
         try:
             loc = get_latlon_coordinates(station_number)
             if 0. not in loc:
@@ -87,22 +112,36 @@ def build_station_json(data):
 
 
 def write_jsons(data):
+    if not os.path.exists('data/'):
+        os.mkdir('data')
+
+    for subcluster in Network().subclusters():
+        subcluster_name = subcluster['name'].lower().replace(' ', '_')
+
+        stations = build_station_json(data, subcluster['number'])
+        with open('data/stations_' + subcluster_name + '.json', 'w') as f:
+            json.dump(stations, f)
+
+        coincidences = build_coincidence_json(data, subcluster_name)
+        with open('data/coincidences_' + subcluster_name + '.json', 'w') as f:
+            json.dump(coincidences, f)
+
     stations = build_station_json(data)
-    with open('stations.json', 'w') as f:
-        json.dump(stations, f, indent=4)
+    with open('data/stations_network.json', 'w') as f:
+        json.dump(stations, f)
 
     coincidences = build_coincidence_json(data)
-    with open('coincidences.json', 'w') as f:
-        json.dump(coincidences, f, indent=4)
+    with open('data/coincidences_network.json', 'w') as f:
+        json.dump(coincidences, f)
 
     for station in STATIONS:
         events = build_events_json(data, station)
-        with open('events-s%d.json' % station, 'w') as f:
-            json.dump(events, f, indent=4)
+        with open('data/events_s%d.json' % station, 'w') as f:
+            json.dump(events, f)
 
 
 def get_latlon_coordinates(station_number):
-    station = Station(station_number)
+    station = Station(station_number, date=START)
     gps_location = station.location()
     if gps_location['latitude'] == 0.:
         raise Exception
@@ -113,5 +152,7 @@ if __name__ == '__main__':
     if 'data' not in globals():
         data = tables.open_file('data.h5', 'w')
 
-    download_data_coincidences(data)
+    download_events_data(data)
+    download_coincidences_data(data)
+
     write_jsons(data)
