4dde6f9700faae5117330fc9edb7915775a7c6b0
Tom Kooij <tomkooij@tomkooij.nl>
2019-07-02 14:14:48 +0200

    Add singles to data retrieval tool

--- jquery.jsparc.js
+++ jquery.jsparc.js
@@ -59,6 +59,17 @@ be stored as strings.
                               'heat_index': {'column': 14, 'name': 'Heat index', 'units': '°C'},
                               'dew_point': {'column': 15, 'name': 'Dew point', 'units': '°C'},
                               'wind_chill': {'column': 16, 'name': 'Wind chill', 'units': '°C'}},
+            singles_format = {'date': {'column': 0, 'name': 'Date', 'units': 'GPS date'},
+                              'time': {'column': 1, 'name': 'Time', 'units': 'GPS time'},
+                              'timestamp': {'column': 2, 'name': 'Timestamp', 'units': 's'},
+                              'mas_ch1_high': {'column': 3, 'name': 'ch1 high', 'units': 'Hz'},
+                              'mas_ch1_low': {'column': 4, 'name': 'ch1 low', 'units': 'Hz'},
+                              'mas_ch2_high': {'column': 5, 'name': 'ch2 high', 'units': 'Hz'},
+                              'mas_ch2_low': {'column': 6, 'name': 'ch2 low', 'units': 'Hz'},
+                              'slv_ch1_high': {'column': 7, 'name': 'ch3 high', 'units': 'Hz'},
+                              'slv_ch1_low': {'column': 8, 'name': 'ch3 low', 'units': 'Hz'},
+                              'slv_ch2_high': {'column': 9, 'name': 'ch4 high', 'units': 'Hz'},
+                              'slv_ch2_low': {'column': 10, 'name': 'ch4 low', 'units': 'Hz'}},
             lightning_format = {'date': {'column': 0, 'name': 'Date', 'units': 'GPS date'},
                                 'time': {'column': 1, 'name': 'Time', 'units': 'GPS time'},
                                 'timestamp': {'column': 2, 'name': 'Timestamp', 'units': 's'},
@@ -123,6 +134,7 @@ be stored as strings.
         jsparc.JSPARC_URL = JSPARC_URL;
         jsparc.events_format = events_format;
         jsparc.weather_format = weather_format;
+        jsparc.singles_format = singles_format;
         jsparc.lightning_format = lightning_format;
         jsparc.eventtime_format = eventtime_format;
         jsparc.pulseheight_format = pulseheight_format;
@@ -435,6 +447,8 @@ be stored as strings.
                 url = api_stations_with_data();}
             else if (type == 'weather') {
                 url = api_stations_with_weather();}
+            else if (type == 'singles') {
+                url = api_stations_with_singles();}
             else {
                 url = api_stations();}
             return get_json(url)
@@ -732,6 +746,10 @@ be stored as strings.
         jsparc.api_stations_with_weather = api_stations_with_weather;
         function api_stations_with_weather(year, month, day) {
             return build_url([API_URL, 'stations/weather', year, month, day, '']);}
+
+        jsparc.api_stations_with_singles = api_stations_with_singles;
+        function api_stations_with_singles(year, month, day) {
+            return build_url([API_URL, 'stations/singles', year, month, day, '']);}
 
         jsparc.api_station_info = api_station_info;
         function api_station_info(station_number, year, month, day) {
